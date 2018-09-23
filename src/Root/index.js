3f2af8a4591822dfba052e618bc794f83da38d78
Thiago Oliveira <thiago@signaes.com>
2018-09-23 16:06:13 -0300

    Add responsive meta tag

--- src/Root/index.js
+++ src/Root/index.js
@@ -1,12 +1,16 @@
 // @flow
 import React from 'react';
 import { Route, Switch } from 'fusion-plugin-react-router';
+import { Helmet } from 'fusion-plugin-react-helmet-async';
 import Home from '../pages/Home/index.js';
 import PageNotFound from '../pages/pageNotFound.js';
 import GlobalStyle from './styles';
 
 const root = (
   <React.Fragment>
+    <Helmet>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Helmet>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route component={PageNotFound} />
--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 // @flow
 import App from 'fusion-react';
 import Router from 'fusion-plugin-react-router';
+import HelmetPlugin from 'fusion-plugin-react-helmet-async';
 import Styletron from 'fusion-plugin-styletron-react';
-
+import I18n, {
+  I18nToken,
+  I18nLoaderToken,
+  createI18nLoader,
+} from 'fusion-plugin-i18n-react';
+import { FetchToken } from 'fusion-tokens';
+import fetch from 'unfetch';
 import Root from './Root/index';
 
 export default () => {
   const app = new App(Root);
   app.register(Styletron);
   app.register(Router);
+  app.register(HelmetPlugin);
+  // app.register(I18nToken, I18n);
+
+  // __NODE__
+  //   ? app.register(I18nLoaderToken, createI18nLoader())
+  //   : app.register(FetchToken, fetch);
+
   return app;
 };
