9e784fd497d2d9eeaf0c69bfd518902480a21d07
Thiago Oliveira <thiago@signaes.com>
2018-09-23 12:34:46 -0300

    Add CSS reset

--- src/Root/styles.js
+++ src/Root/styles.js
@@ -1,8 +1,11 @@
 //@flow
 import * as React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+
   @import url('https://fonts.googleapis.com/css?family=Varela+Round');
 
   body {
